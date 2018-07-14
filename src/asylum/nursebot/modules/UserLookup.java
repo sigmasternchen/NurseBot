6318643e3f2d027f97dd8f151b73903e8ee4e74d
overflowerror <mail@overflowerror.com>
2018-07-15 00:38:12 +0200

    User Lookup finished

--- src/asylum/nursebot/modules/UserLookup.java
+++ src/asylum/nursebot/modules/UserLookup.java
@@ -1,8 +1,12 @@
 package asylum.nursebot.modules;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.javalite.activejdbc.Base;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.MessageEntity;
 import org.telegram.telegrambots.api.objects.User;
 
 import com.google.inject.Inject;
@@ -23,13 +27,51 @@ import asylum.nursebot.semantics.SemanticInterpreter;
 import asylum.nursebot.semantics.SemanticsHandler;
 import asylum.nursebot.semantics.WakeWord;
 import asylum.nursebot.semantics.WakeWordType;
-import asylum.nursebot.utils.MessageUtils;
 import asylum.nursebot.utils.StringTools;
 
 @AutoModule(load=true)
 @AutoDependency
 public class UserLookup implements Module {
 
+	class MinimalUser extends User {
+		private static final long serialVersionUID = 2210503080750916556L;
+		
+		private String name;
+		private int id;
+		
+		public MinimalUser(int id, String name) {
+			this.name = name;
+			this.id = id;
+		}
+		
+		@Override
+		public String getUserName() {
+			if (name.startsWith("@"))
+				return name;
+			return null;
+		}
+		
+		@Override
+		public String getFirstName() {
+			return name;
+		}
+		
+		@Override
+		public Integer getId() {
+			return id;
+		}
+		
+		@Override
+		public Boolean getBot() {
+			return null;
+		}
+		
+		@Override
+		public String getLanguageCode() {
+			return null;
+		}
+	}
+	
 	@Inject
 	private SemanticsHandler semanticsHandler;
 	
@@ -86,14 +128,21 @@ public class UserLookup implements Module {
 				.setVisibility(Visibility.PRIVATE)
 				.setCategory(category)
 				.setAction(c -> {
-					List<User> users = MessageUtils.getMentionedUsers(c.getMessage());
+					User user;
 					
-					if (users.size() == 0) {
-						c.getSender().reply("Deine UserID ist " + c.getMessage().getFrom().getId() + ".", c.getMessage());
+					if (c.getMessage().isReply()) {
+						user = c.getMessage().getReplyToMessage().getFrom();
 					} else {
-						User user = users.get(0);
-						c.getSender().reply("Der User " + StringTools.makeMention(user) + " hat die ID " + user.getId() + ".", c.getMessage(), true);
+						List<User> users = getMentions(c.getMessage());
+						if (users.size() == 0) {
+							user = c.getMessage().getFrom();	
+						} else {
+							user = users.get(0);
+						}
 					}
+					
+					c.getSender().reply("Der User " + StringTools.makeMention(user) + " hat die ID " + user.getId() + ".", c.getMessage(), true);
+					
 				}));
 	}
 
@@ -121,10 +170,71 @@ public class UserLookup implements Module {
 	}
 	
 	public Integer getUserId(String username) {
+		if (username.startsWith("@"))
+			username = username.substring(1);
+		
 		UserLookupEntry entry = UserLookupEntry.getByUsername(username);
 		if (entry == null)
 			return null;
 		return (Integer) entry.getUserid();
+	}
+	
+	public List<Integer> getMentionIds(Message message) {
+		List<Integer> list = getUsernameMentionIds(message);
+		list.addAll(getNonUsernameMentionIds(message));
+		return list;
+	}
+	
+	public List<User> getMentions(Message message) {
+		List<User> list = getUsernameMentions(message);
+		list.addAll(getNonUsernameMentions(message));
+		return list;
+	}
+	
+	private List<User> getUsernameMentions(Message message) {
+		List<User> list = new LinkedList<>();
+		
+		for (MessageEntity entity : message.getEntities()) {
+			if (!entity.getType().equals("mention"))
+				continue;
+			
+			Integer user = getUserId(entity.getText());
+			if (user != null)
+				list.add(new MinimalUser(user, entity.getText()));
+		}
+		
+		return list;
+	}
+	
+	private List<Integer> getUsernameMentionIds(Message message) {
+		List<Integer> list = new LinkedList<>();
+		
+		for (MessageEntity entity : message.getEntities()) {
+			if (!entity.getType().equals("mention"))
+				continue;
+			
+			Integer user = getUserId(entity.getText());
+			if (user != null)
+				list.add(user);
+		}
+		
+		return list;
+	}
+	
+	private List<User> getNonUsernameMentions(Message message) {
+		List<User> list = new LinkedList<>();
+		
+		for (MessageEntity entity : message.getEntities()) {
+			if (!entity.getType().equals("text_mention"))
+				continue;
+			list.add(entity.getUser());
+		}
+		
+		return list;
+	}
+	
+	private List<Integer> getNonUsernameMentionIds(Message message) {
+		return getNonUsernameMentions(message).stream().map(u -> u.getId()).collect(Collectors.toList());
 	}
 	
 	@Override
