d93128f162fbc04fda24fbb37a881c245ff5fae9
overflowerror <mail@overflowerror.com>
2018-11-02 23:25:44 +0100

    SemanticAction is a functional interface

--- src/asylum/nursebot/semantics/SemanticAction.java
+++ src/asylum/nursebot/semantics/SemanticAction.java
@@ -2,6 +2,7 @@ package asylum.nursebot.semantics;
 
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+@FunctionalInterface
 public interface SemanticAction {
 	void action(SemanticContext context) throws TelegramApiException;
 }
