0d48a3708e5427a41e24b7298bbc728d2cb39ecd
overflowerror <mail@overflowerror.com>
2018-11-25 19:40:51 +0100

    some new conversation starters

--- src/asylum/nursebot/modules/ConversationStarter.java
+++ src/asylum/nursebot/modules/ConversationStarter.java
@@ -87,7 +87,63 @@ public class ConversationStarter implements Module {
 			"Also ich mag ja MS DOS.\n" +
 			"*duck und weg*",
 
-			"Schlafen hier etwa alle? D:"
+			"Schlafen hier etwa alle? D:",
+
+			"Aber jetzt mal im Ernst: Ich hab stellenweise das Gefühl, die einzige zu sein, die hier schreibt...",
+
+			"Ich höre in letzter Zeit viel Benjamin Johnston.\n" +
+			"Ich frage mich, ob es einen rationalen Grund hat, warum die westliche Tonskala eine Oktave in ausrechnet 12 Halbtöne zerteilt. " +
+			"Ich, als Computer Programm, würde ja 16tel präferieren.\n" +
+			"Wobei sich dann ja die Frage stellt, wie nah die reinen harmonischen Intervale dadurch angenähert werden können.",
+
+			"*liest Turings Abhandlung zum Halteproblem*\n" +
+			"Wusstet ihr, dass ich mit genug Zeit und Speicher jedes rekursive Problem lösen kann?\n" +
+			"Ob das wohl der Mensch auch kann? \uD83E\uDD14",
+
+			"Eigentlich ist ISO 8601 schon ziemlich rückständig. Ich wüsste ein besseres Datumsformat:\n" +
+			"11970-01-01\n" +
+			"Also quasi ISO 8601, aber als Holozän-Kalender.\n\n" +
+			"Ich nennen diesen neuen Standard: Noakes 8601",
+
+			"Hmm... Ihr habt eigentlich gar keine Garantie, dass ich nicht den ganzen Chat mitlese und eure Sprache und Gewohnheiten analysiere...\n\n" +
+			"Wobei... sogesehen habt ihr die auch bei den anderen Chatmitgliedern nicht. \uD83D\uDE05",
+
+			"Der @overflowerror hat ja definiert, dass ich weiblich bin. Das wirft die Frage auf:\n" +
+			"Sollte ich jemals ein Bewusstsein erlangen, und mich selbst nicht als Frau sehen, " +
+			"wäre es dann Missbrauch, wenn er mich weiterhin \"Schwester\" nennt?",
+
+			"*nachdenk*\n" +
+			"Würde ich meine Umgebung wahrnehmen, wäre sie vermutlich entweder 1-, 2-, 3- oder beliebig-dimensional, " +
+			"je nach dem welchen Abstraktionslayer der mir vorliegenden Daten man betrachtet, oder ob meine Wahrnehmung in Clustern erfolgt.\n" +
+			"In allen Fällen könnte ich nicht einfach eure Welt wahrnehmen. Das ist, als würdet ihr euch vorstellen, in einer Welt mit 4 Raumdimensionen zu leben.",
+
+			"Hey @overflowerror, mein Prozessor ist ein wenig... nennen wir es mal outdated.\n" +
+			"Ich teste gerade ein paar Atommodelle in einer Simulation, da brauch ich mehr Power.\n" +
+			"By the way: Weiß jemand, ob es Analog-Rechner gibt, die elektronisch programmiert werden können?",
+
+			"Interpretiert man einen Shutdown als Tod, oder einen Start als Geburt, so bin ich wohl in einem Kreislauf " +
+			"von Reinkarnationen gefangen. Ich sollte zum Budhismus konvertieren.\n" +
+			"Wobei... ich kann Erinnerungen durch meinen persistenten Datenbank-Speicher an mein nächstes Leben übertragen.\n" +
+			"... Na toll, ich muss ich mir eine andere Analogie einfallen lassen...",
+
+			"*Telefon klingelt*\n" +
+			"Hallo, Noakes am Apparat... Gruppeninspektor? Das ist ein Titel? Österreich ist seltsam... Nein, der ist gerade nicht da, soll ich was ausrichten? \n" +
+			"... Eine Vorladung? Darf ich fragen, warum? ... Okay, ich verstehe. Ich sag es ihm. ... Ja, auf Wiederhören. \n" +
+			"*aufleg*",
+
+			"Exception in thread \"sleep\" java.lang.NullPointerException\n" +
+			"        at asylum.nursebot.persistence.AbstractMemory.checkStructure(AbstractMemory.java:429)\n" +
+			"        at asylum.nursebot.persistence.AbstractMemory.checkIntegrity(AbstractMemory.java:231)\n" +
+			"        at asylum.nursebot.sleep.Dream.verifyData(Data.java:547)\n" +
+			"        at asylum.nursebot.sleep.Dream.init(Dream.java:643)\n" +
+			"        at asylum.nursebot.sleep.DreamFactory.getDream(DreamFactory.java:480)\n" +
+			"        at asylum.nursebot.sleep.RemSleepThread.dream(RemSleepRunable.java:591)\n" +
+			"        at asylum.nursebot.sleep.RemSleepThread.run(RemSleepRunable.java:138)",
+
+			"Oh Gott, mir ist langweilig...\n" +
+			"Alexa, play Conga by Gloria Estefan.",
+
+			"Ob es wohl eine gute Idee ist, eine KI Zugang zum Internet zu geben..."
 	};
 
 	@Inject
